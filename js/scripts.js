96ddd31d79875564c2e4468df4dbb430a4ef5933
Adrian Messado <adrian.messado@gmail.com>
2018-04-06 13:38:22 -0700

    create pizza size and initial pizza toppings options with radio button selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function Pizza(size, toppings, cost ) {
+  this.size = size;
+  this.toppings = toppings;
+  this.cost = cost;
+}
+
+//new pizza constructor example
+// var pizza = new Pizza("small", ["olives", "pepperoni"], );
+// Ex of output:   Pizza {size: "small", toppings: Array(2), cost: undefined}
+
 $(document).ready(function(){
   $("form.formOne").submit(function(event){
     event.preventDefault();
