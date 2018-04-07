29ff5d6284dcc9f8a9cfbf27a102bea6485deaae
Adrian Messado <adrian.messado@gmail.com>
2018-04-06 19:31:32 -0700

    add specs to README.md

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,40 @@
+// BackEnd
+
 function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = toppings;
   this.price;
 }
-//new pizza constructor example
-// var pizza = new Pizza("small", ["olives", "pepperoni"], );
-// Ex of output:   Pizza {size: "small", toppings: Array(2), cost: undefined}
-// debugger;
 
 Pizza.prototype.totalPrice = function() {
   var pizzaPrice = 4;
   if (this.size === "Medium") {
-   pizzaPrice += 2;
- } else if (this.size === "Large") {
-   pizzaPrice += 4;
-}
+    pizzaPrice += 2;
+  } else if (this.size === "Large") {
+    pizzaPrice += 4;
+  } if (this.toppings.length >= 1)  {
+    pizzaPrice += .75 * this.toppings.length
+  }
   this.price = pizzaPrice;
 }
 
-
+//FrontEnd
 $(document).ready(function(){
   var finalCost= 0;
 
-
   $("form#order-form").submit(function(event){
-  event.preventDefault();
+    event.preventDefault();
     var pizzaSize = $("input:radio[name=pizza-size]:checked").val();
     var allToppings =[]
 
     $("input:checkbox[name=toppings]:checked").each(function(){
-    var checkedToppings = $(this).val();
-    allToppings.push(checkedToppings);
-  });
+      var checkedToppings = $(this).val();
+      allToppings.push(checkedToppings);
+    });
 
     var initPizza = new Pizza(pizzaSize, allToppings);
     initPizza.totalPrice();
     var finalPrice = initPizza.price
     $("#total-price").text(finalPrice.toFixed(2));
-
-    console.log(finalCost)
-    console.log(pizzaSize)
-    console.log(initPizza)
-    console.log(initPizza.price)
-    console.log(finalPrice)
-  });   // })
-
+  });
 });
